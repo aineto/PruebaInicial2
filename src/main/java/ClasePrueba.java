ec965ca5156dfbba0a1382f7d09483f377bb2a61
Aineto <Aineto@Aineto-PC>
2021-07-21 09:23:48 +0200

    He creado un proecto

---
+++ src/main/java/ClasePrueba.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Aineto
+ */
+public class ClasePrueba {
+    
+    public int suma(int a , int b){
+        return a+b;
+    }
+    
+}
